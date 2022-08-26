160b6e08931ba5fbecd459c9f3a886a1ccf99fa9
Motcrash <89164397+Motcrash@users.noreply.github.com>
2022-08-25 18:48:49 -0600

    Add files via upload

---
+++ EVA1/EVA1_7_Medir_tiempo/src/eva1_7_medir_tiempo/EVA1_7_Medir_tiempo.java
@@ -0,0 +1,16 @@
+package eva1_7_medir_tiempo;
+
+//   @author Gerardo Delgado
+public class EVA1_7_Medir_tiempo {
+    public static void main(String[] args) {
+        long ini = System.nanoTime();
+        int[] data = new int[100000000];
+        for (int i : data) {
+            data[i] = 100;
+        }
+        long fin = System.nanoTime();
+        long tiempo = fin - ini;
+        System.out.println((double)tiempo/1000000000);
+    }
+    
+}
